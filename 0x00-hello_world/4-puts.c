d8752b3768a775fd891c682cb9d7cbc2be98b65b
Baho Joselyto <88556861+Josy-coder@users.noreply.github.com>
2022-09-08 11:56:31 +0200

    Created and updated README, Task 0 - Task 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C file that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
