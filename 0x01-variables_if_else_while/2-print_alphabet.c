7dc6536f0cccb55fcf775635df26929afbabb0f7
Baho Joselyto <88556861+Josy-coder@users.noreply.github.com>
2022-09-09 17:03:20 +0200

    Created and updated README and mandatory tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * programming - prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * program - prints the alphabet in lowercase, followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		if (al != 'e' && al != 'q')
+		{
+			putchar(al);
+		}
+		al++;
+	}
+	putchar('\n');
+	return (0);
+}
